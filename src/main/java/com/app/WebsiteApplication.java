f646241fd265faa9cb83b42df670c95d7523a5a0
vishalpawar1234 <152585025+vishalpawar1234@users.noreply.github.com>
2024-05-13 11:55:09 +0530

    Update WebsiteApplication.java

--- src/main/java/com/app/WebsiteApplication.java
+++ src/main/java/com/app/WebsiteApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+	@ComponentScan("com.app.controller")
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
